50cdca205fd2d3c67a1a0a9485820ab016b01e3b
Arthur <gloriouslordbear@outlook.com>
2018-04-17 13:30:11 -0300

    bleh

--- TP01/app/src/main/java/br/pucminas/arthur/lddm/tp01/ContactCreation.java
+++ TP01/app/src/main/java/br/pucminas/arthur/lddm/tp01/ContactCreation.java
@@ -29,10 +29,11 @@ import org.androidannotations.annotations.ViewById;
 import org.androidannotations.annotations.res.LayoutRes;
 
 import java.util.Calendar;
+import java.util.Date;
 import java.util.TimeZone;
 
 @EActivity(R.layout.activity_contact_creation)
-public class ContactCreation extends AppCompatActivity implements View.OnClickListener {
+public class ContactCreation extends AppCompatActivity implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
 
     @ViewById(R.id.editText1)
     public EditText name;
@@ -47,6 +48,7 @@ public class ContactCreation extends AppCompatActivity implements View.OnClickLi
     public EditText birthday;
 
     private DatePickerDialog datepicker;
+    private int year, month, day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,95 +71,47 @@ public class ContactCreation extends AppCompatActivity implements View.OnClickLi
     }
 
     private void pickDate() {
-        datepicker = new DatePickerDialog(this, R.style.Theme_AppCompat_Light_Dialog, new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                birthday.setText(Integer.toString(dayOfMonth)+"/"+Integer.toString(month)+"/"+Integer.toString(year));
-            }
-        }, 2018, 01, 01);
+        datepicker = new DatePickerDialog(this, R.style.Theme_AppCompat_Light_Dialog, this, 2018, 01, 01);
         datepicker.show();
     }
 
     private void sendForm() {
-        Contact contato = new Contact(name.getText().toString(), email.getText().toString(), phone.getText().toString(), Calendar.getInstance());
-//        Intent intent = new Intent(Intent.ACTION_INSERT);
-//        intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
-//
-//        intent.putExtra(ContactsContract.Intents.Insert.NAME, contato.getName());
-//        intent.putExtra(ContactsContract.Intents.Insert.PHONE, contato.getPhone());
-//        intent.putExtra(ContactsContract.Intents.Insert.EMAIL, contato.getEmail());
-//        //intent.putExtra(ContactsContract.Intents.Insert., contato.getEmail());
-//
-//
-//        startActivityForResult(intent, 0);
-        sendWppMessage();
+        if(validFields()) {
+            Contact contato = new Contact(name.getText().toString(), email.getText().toString(), phone.getText().toString(), Calendar.getInstance());
+            Intent intent = new Intent(Intent.ACTION_INSERT);
+            intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+            intent.putExtra(ContactsContract.Intents.Insert.NAME, contato.getName());
+            intent.putExtra(ContactsContract.Intents.Insert.PHONE, contato.getPhone());
+            intent.putExtra(ContactsContract.Intents.Insert.EMAIL, contato.getEmail());
 
-//        PackageManager pm=getPackageManager();
-//        try {
-//            intent = new Intent(Intent.ACTION_SEND);
-//            intent.setType("text/plain");
-//            String text = "Olá, " + name.getText().toString() + ", adicionei seu número a minha lista de contatos";
-//            PackageInfo info = pm.getPackageInfo("com.whatsapp", PackageManager.GET_META_DATA);
-//            //Check if package exists or not. If not then code
-//            //in catch block will be called
-//            intent.setPackage("com.whatsapp");
-//        } catch (PackageManager.NameNotFoundException e) {
-//            Toast.makeText(getApplicationContext(), "WhatsApp não instalado.", Toast.LENGTH_LONG);
-//        }
-
-// provide an initial time
-
+            startActivityForResult(intent, 0);
+        }
     }
 
-    /*public void createEvent(){
+    public void createEvent(){
+        Date date = new Date(year, month, day);
         Intent intent = new Intent(Intent.ACTION_EDIT);
         intent.setType("vnd.android.cursor.item/event");
-        intent.putExtra("beginTime", begintime.getTimeInMillis());
-        intent.putExtra("allDay", true);
-        intent.putExtra("rrule", "FREQ=YEARLY");
-        intent.putExtra("endTime", endtime.getTimeInMillis()+60*60*1000);
-        intent.putExtra("title", "A Test Event from android app");
-        startActivity(intent);
-    }*/
+        intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, date.getTime());
+        intent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
+        intent.putExtra(CalendarContract.Events.RRULE, "FREQ=YEARLY");
+        intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, date.getTime()+60*60*1000);
+        intent.putExtra(CalendarContract.Events.TITLE, "A Test Event from android app");
+        startActivityForResult(intent, 1);
+    }
 
     @Override
     protected void onActivityResult(int request, int result, Intent data){
         super.onActivityResult(request, result, data);
-        sendWppMessage();
-        /*if(request == 0){
-            Intent intent = new Intent(Intent.ACTION_INSERT);
-            //intent.setType(CalendarContract.Events.)
-            ContentResolver cr = getContentResolver();
-            ContentValues values = new ContentValues();
 
-// provide an initial time
-            long now = System.currentTimeMillis();
-            values.put(CalendarContract.Events.DTSTART, now);
-            values.put(CalendarContract.Events.DTEND, now + 3600000);
-            values.put(CalendarContract.Events.TITLE, "");
-            values.put(CalendarContract.Events.CALENDAR_ID, CalendarContract.Calendars.IS_PRIMARY);
-            values.put(CalendarContract.Events.EVENT_TIMEZONE, TimeZone.getDefault().getID());
-
-// create the "placeholder" event
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
-
-// get the event ID that is the last element in the Uri
-            long eventId = Long.parseLong(uri.getLastPathSegment());
-
-// launch the editor to edit the "placeholder" event
-            intent.setData(ContentUris.withAppendedId(CalendarContract.Events.CONTENT_URI, eventId));
-            startActivityForResult(intent, 1);
-        }*/
+        switch (request){
+            case 0:
+                createEvent();
+                break;
+            case 1:
+                sendWppMessage();
+                break;
+        }
 
     }
 
@@ -176,4 +130,20 @@ public class ContactCreation extends AppCompatActivity implements View.OnClickLi
         }
     }
 
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        birthday.setText(Integer.toString(dayOfMonth)+"/"+Integer.toString(month)+"/"+Integer.toString(year));
+        this.day = dayOfMonth;
+        this.month = month;
+        this.year = year;
+    }
+
+    private boolean validFields(){
+        if(name.getText().toString().isEmpty() || name.getText().toString() == "") return false;
+        else if(phone.getText().toString().isEmpty() || phone.getText().toString() == "") return false;
+        else if(email.getText().toString().isEmpty() || email.getText().toString() == "") return false;
+        else if(birthday.getText().toString().isEmpty() || birthday.getText().toString() == "") return false;
+        else return true;
+    }
 }
+
